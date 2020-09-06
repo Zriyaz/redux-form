84a3ed535bd32806daabf99d76c12137e03e451d
kazmi786 <riyazkazmi59059@gmail.com>
2020-09-06 22:59:53 +0530

    completed

---
+++ src/SampleCode.js
@@ -0,0 +1,32 @@
+// Array based approach
+const streamReducer = (stste=[], action) =>{
+
+	switch(action.type){
+		case 'EDIT_STREAM':
+		return state.map(stream =>{
+			if(stream.id === action.payload.id){
+				return action.payload
+			}else{
+				return stream
+			}
+		})
+	   default: 
+	   return state
+	}
+}
+
+
+// Object based Approach
+const streamReducer = (state={}, action) =>{
+
+	switch(action.type){
+		case 'EDIT_STREAM':
+		  /*const newState = {...state}
+		  newState[action.payload.id] = action.payload
+		  return newState*/
+
+		  return {...state, [action.payload.id]: action.payload}
+		default:
+		return state
+	}
+}
